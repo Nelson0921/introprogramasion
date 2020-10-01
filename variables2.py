125bd710617dc7e7801e1de4c690e9395c48ebdb
NELSON <nelsonvaca31@gmail.com>
2020-10-01 16:12:34 -0400

    ejercicio 1 variables2.py

---
+++ variables2.py
@@ -0,0 +1,4 @@
+# E1: Leer un numero por teclado que representa una longitud en millas y mostrar el valor equivalente en kilomentros en pantalla
+distancia_millas=float("ingrese la distancia en millas:"))
+distancia_kilometros= distancia_millas * 1.60934
+print(f"{distancia_millas} millas equivalen a {distancia_kilometros} kilometros")
