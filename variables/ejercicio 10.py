fbf6b329307d925b2cc2e68c61603c2f8b6c0da0
NELSON <nelsonvaca31@gmail.com>
2020-10-02 16:30:51 -0400

    ejercicio 10

---
+++ variables/ejercicio 10.py
@@ -0,0 +1,8 @@
+n1 = int(input ("ingrese un numero: "))
+n2 = int(input("ingrese un segundo numero: "))
+n3 = int(input("ingrese un tercer numero: "))
+operacion_1 = n1 * int(100)
+operacion_2 = n2 * int(10)
+operacion_3 = operacion_2 + operacion_1 +n3
+if operacion_3  == int(777
+    print("ganaste!")
