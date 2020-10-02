ebf01239b027c16e7a9917040d436ab5c0d64c5e
NELSON <nelsonvaca31@gmail.com>
2020-10-02 14:43:54 -0400

    ejercicio3.py

---
+++ variables/EJERCICIO 6.py
@@ -0,0 +1,4 @@
+peso = float(input("¿ingresa tu peso en kg?"))
+altura = float(input("¿ingresa tu altura en metros"))
+indicedemasacorporal = float(peso/(altura**2))
+print("tu indice de masa corporal es",indicedemasacorporal)
