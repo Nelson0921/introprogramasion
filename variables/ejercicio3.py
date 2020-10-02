3f9a26719cbe5abd62fff092b96b7f4bc21b851f
NELSON <nelsonvaca31@gmail.com>
2020-10-02 16:55:05 -0400

    ejercicio 2

---
+++ variables/ejercicio3.py
@@ -0,0 +1,8 @@
+partido1 = "mas"
+partido2 = "unir"
+print(partido1, partido2)
+voto = input("seleccione un partido: ")
+if voto == partido1 or voto ==partido2
+    print("usted voto")
+else :
+    print("voto invalido")
