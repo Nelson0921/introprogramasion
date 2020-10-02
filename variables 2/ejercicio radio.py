8ddfc3b9d3c3adf2a0143a19747fff6bf3835a55
NELSON <nelsonvaca31@gmail.com>
2020-10-02 14:24:40 -0400

    ejercicio3.py

---
+++ variables 2/ejercicio radio.py
@@ -0,0 +1,10 @@
+unidad = str(input("¿En que unidad de media tienes tu radio? "))
+radio = float(input("dame el radio del circulo: "))
+perimetro = (2*3.1415*radio)
+area = (3.1415*radio**2)
+print("el radio es igual a: ", radio, unidad,"perimetro", perimetro, unidad, "area",area,unidad,"**2")
+
+
+
+
+
