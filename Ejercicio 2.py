516fee0aab9f2a45eaeb33007ba3a8d2045e35ce
NELSON <nelsonvaca31@gmail.com>
2020-10-02 12:04:51 -0400

    ejercicio3.py

---
+++ Ejercicio 2.py
@@ -0,0 +1,5 @@
+dividendo =int(input("dame el dividendo: "))
+divisor = int(input("dame el divisor: "))
+resto = dividendo % divisor
+respuesta = f"El resto obtenido al dividir {dividendo} entre {resto}"
+print(respuesta)
